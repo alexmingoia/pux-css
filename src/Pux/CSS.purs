66aa05cbaf32205e90fc082cef6d76df0e10632b
Alex Mingoia <talk@alexmingoia.com>
2016-06-26 14:17:37 -0700

    Update for PureScript 0.9

--- src/Pux/CSS.purs
+++ src/Pux/CSS.purs
@@ -10,22 +10,23 @@ import Data.Foldable (foldl)
 import Data.Monoid (mempty)
 import Data.String (drop, joinWith, split, take, toUpper, toLower)
 import Data.Tuple (Tuple(Tuple))
-import Prelude (($), (++), map)
+import Prelude (($), (<>), map)
 import Pux.Html (Attribute)
+import Pux.Html.Attributes (style) as A
 
 style :: forall a. CSS -> Attribute a
-style rules = Pux.Html.Attributes.style $ css rules
+style rules = A.style $ css rules
 
 css :: CSS -> Array (Tuple String String)
 css rules =
   map tuple $ foldl tuples [] $ runS rules
   where
   tuple p = either (\_ -> mempty) (\(Tuple k v) -> Tuple (camelCase k) v) p
-  tuples array (Property key val) = collect (Tuple key val) ++ array
+  tuples array (Property key val) = collect (Tuple key val) <> array
   tuples array _ = array
 
 camelCase :: String -> String
-camelCase str = toLower (take 1 pascalCase) ++ (drop 1 pascalCase)
+camelCase str = toLower (take 1 pascalCase) <> (drop 1 pascalCase)
   where
   pascalCase = joinWith "" $ map capitalize (split "-" str)
-  capitalize word = toUpper (take 1 word) ++ (drop 1 word)
+  capitalize word = toUpper (take 1 word) <> (drop 1 word)
